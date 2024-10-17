e9bde096f714d499160ddfaf359baf9e12c78f2e
sowmiyajana9797 <sowmiya.janar@gmail.com>
2024-10-17 11:55:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("input");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+} 
+
+
+
+
